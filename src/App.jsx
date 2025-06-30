e5d74b1c2890b1e1c1c252d8cd3d17628e9f75df
Anshul0621 <anshulsharmax1@gmail.com>
2025-07-01 02:46:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl text-white bg-black' >
+
+      hiii there
+      
+    </div>
+  )
+}
+
+export default App 
