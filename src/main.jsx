5bb0e7ef86725dc67934b39bebbfbd3a41e295fe
Anshul0621 <anshulsharmax1@gmail.com>
2025-07-01 02:59:39 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import Context from './assets/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Context >
